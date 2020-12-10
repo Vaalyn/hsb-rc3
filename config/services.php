10686e1bc42f3188747832a968f8104cff4a8510
Vaalyn <vaalyndev@gmail.com>
2020-12-11 00:07:45 +0100

    Enable template caching and remove debug

--- config/services.php
+++ config/services.php
@@ -10,8 +10,7 @@ return [
     Slim\Views\Twig::class => function() {
         $twig = Slim\Views\Twig::create(
             __DIR__ . '/../templates',
-            //['cache' => __DIR__ . '/../var/cache']
-            ['debug' => true]
+            ['cache' => __DIR__ . '/../var/cache']
         );
 
         $twig->addExtension(new \Twig\Extension\DebugExtension());
